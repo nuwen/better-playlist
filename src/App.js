18d5f75ac79b45508721fdd59dc8f259fbc733a9
Danton Nguyen <dantonnguyen88@gmail.com>
2018-11-30 22:47:59 -0800

    add fake server data to playlists and logic to aggregates

--- src/App.js
+++ src/App.js
@@ -7,12 +7,93 @@ let defaultStyle = {
 
 }
 
+let fakeServerData = {
+    user: {
+        name: "David",
+        playlists: [
+          {
+            name: 'My favourites',
+            songs: [
+              {
+                name: 'Beat It',
+                duration: 1242
+              },
+              {
+                name: 'Canelloni Makaroni',
+                duration: 1323
+              },
+              {
+                name: 'Rose Helicopter',
+                duration: 1831
+              }
+            ]
+          },
+          {
+            name: 'Discover Weekly',
+            songs: [
+              {
+                name: 'The song',
+                duration: 1324
+              },
+              {
+                name: 'A song',
+                duration: 1289
+              },
+              {
+                name: 'Songs',
+                duration: 1283
+              }
+            ],
+          },
+          {
+            name: 'Are and Bae',
+            songs: [
+              {
+                name: 'That thang',
+                duration: 8939
+              },
+              {
+                name: 'Dipt in the night',
+                duration: 3289
+              }
+            ]
+          },
+          {
+            name: 'Unce-athon',
+            songs: [
+              {
+                name: 'Beep Boop',
+                duration: 3894
+              },
+              {
+                name: 'badump psh',
+                duration: 3991
+              }
+            ]
+          }
+        ]
+    }
+};
 
-class Aggregate extends Component {
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
-        <h3>Number Text</h3>
+        <h3>{this.props.playlists.length} Playlists</h3>
+      </div>
+    )
+  }
+}
+
+class HoursCounter extends Component {
+
+
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, playlist) => songs.concat(playlist.songs), [])
+    let totalDuration = allSongs.reduce((sum, song) => sum + song.duration, 0); 
+    return (
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h3>{Math.round(totalDuration/3600)} Hours</h3>
       </div>
     )
   }
@@ -46,17 +127,35 @@ class Filter extends Component {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      serverData: {}
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData});
+    }, 500)
+  }
   render() {
     return (
       <div className="App App-header">
-        <h1>Title</h1>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+
+        {this.state.serverData.user ?
+        <div>
+          <h1>
+            {this.state.serverData.user.name}'s Playlist
+          </h1>
+          <PlaylistCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
+          <HoursCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists}/>
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> : <h1 style={{...defaultStyle}}>Loading...</h1>
+        }
       </div>
     );
   }
