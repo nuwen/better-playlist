399880a82d81b7b4afdcebb08a35e7acacb56240
Danton Nguyen <dantonnguyen88@gmail.com>
2018-12-01 01:01:19 -0800

    add filter functionality to playlist list

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ class Filter extends Component {
     return (
       <div>
         <img/>
-        <input type="text"/>
+        <input type="text" onKeyUp={event => this.props.onTextChange(event.target.value)}/>
       </div>
     )
   }
@@ -128,13 +128,14 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      serverData: {}
+      serverData: {},
+      filterString: ''
     }
   }
   componentDidMount() {
     setTimeout(() => {
       this.setState({serverData: fakeServerData});
-    }, 500)
+    }, 1000)
   }
   render() {
     return (
@@ -147,9 +148,12 @@ class App extends Component {
           </h1>
           <PlaylistCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
           <HoursCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists}/>
-          <Filter/>
+          <Filter onTextChange={text => this.setState({filterString: text})}/>
           {
-            this.state.serverData.user.playlists.map(playlist => 
+            this.state.serverData.user.playlists
+            .filter(playlist => playlist.name.toLowerCase()
+            .includes(this.state.filterString.toLowerCase()))
+            .map(playlist => 
               <Playlist playlist={playlist} />
           )}
         </div> : <h1 style={{...defaultStyle}}>Loading...</h1>
